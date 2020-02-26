eb7dcfd5660e72a35473615ea8299eacb57126e4
Andrea Berenice Estrada Macias <andrea.estradam21@gmail.com>
2020-02-26 08:34:04 -0500

    Laboratorio 1 DS1-avance

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+using MVCPlantilla.Utilerias;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET
+        public ActionResult  Create(int idVideo, string titulo, int repro, string url)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+            BaseHelper.ejecutarSentencia("INSERT INTO video " + "VALUES (@idVideo, @titulo, @repro, @url)", CommandType.Text, parametros);
+            return View();
+        }
+
+    }
+}
